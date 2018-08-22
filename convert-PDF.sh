02eff83a69f771115554eb28ed2285926b3fd83a
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2018-08-22 12:07:08 +0200

    Removed unused images

--- convert-PDF.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-convert -density 150 $1 -bordercolor black -border 1 -scale 25% ${1%.pdf}_%03d.png
-montage -tile 3x -geometry +5+5 ${1%.pdf}_*.png ${1%pdf}png
-rm -f ${1/.pdf/}_*.png
